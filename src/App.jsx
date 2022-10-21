ec9f17f967dd885150fcc7a811d69e4bc738e5f0
Felipe <dfsilvat@gmail.com>
2022-10-21 15:28:30 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Cabecalho from './components/Cabecalho'
 import Main from './components/Main'
 import NavBar from './components/NavBar'
 
+
+
 function App() {
   return (
     <>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
-    return(
-        <div id='nav'>
-            <div id='container'>
-            <a href='.'>Home</a>
-            <a href='.'>Serviços</a>
-            <a href='.'>Sobre</a>
-            <a href='.'>Contato</a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div id="nav">
+      <div id="container">
+        <a href=".">Home</a>
+        <a href=".">Serviços</a>
+        <a href=".">Sobre</a>
+        <a href=".">Contato</a>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
